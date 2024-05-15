a3dcc5bd7052360708e9bdf02dc7a9c8e5dc963a
gamolong <gaille.amolong1@gmail.com>
2024-05-15 18:42:58 +0800

    Authentication fixes with authentication cookie

--- mygym/lib/db.jsx
+++ mygym/lib/db.jsx
@@ -1,37 +1,46 @@
-'use client';
-import Link from 'next/link';
-import { useFormState } from 'react-dom';
+import sql from 'better-sqlite3';
 
-import { signup } from '@/actions/auth-actions';
+const db = sql('training.db');
 
-export default function AuthForm() {
-  const [formState, formAction] = useFormState(signup, {});
-  return (
-    <form id="auth-form" action={formAction}>
-      <div>
-        <img src="/images/auth-icon.jpg" alt="A lock icon" />
-      </div>
-      <p>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-      </p>
-      <p>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </p>
-      {formState.errors && (
-        <ul id="form-errors">
-          {Object.keys(formState.errors).map((error) => (
-            <li key={error}>{formState.errors[error]}</li>
-          ))}
-        </ul>
-      )}
-      <p>
-        <button type="submit">Create Account</button>
-      </p>
-      <p>
-        <Link href="/">Login with existing account.</Link>
-      </p>
-    </form>
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    email TEXT UNIQUE,
+    password TEXT
   );
+`);
+
+db.exec(`CREATE TABLE IF NOT EXISTS sessions (
+  id TEXT NOT NULL PRIMARY KEY,
+  expires_at INTEGER NOT NULL,
+  user_id TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS trainings (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    image TEXT,
+    description TEXT
+  );
+`);
+
+const hasTrainings =
+  db.prepare('SELECT COUNT(*) as count FROM trainings').get().count > 0;
+
+if (!hasTrainings) {
+  db.exec(`
+    INSERT INTO trainings (title, image, description)
+    VALUES
+    ('Yoga', '/yoga.jpg', 'A gentle way to improve flexibility and balance.'),
+    ('Boxing', '/boxing.jpg', 'A high-energy workout that improves strength and speed.'),
+    ('Running', '/running.jpg', 'A great way to improve cardiovascular health and endurance.'),
+    ('Weightlifting', '/weightlifting.jpg', 'A strength-building workout that helps tone muscles.'),
+    ('Cycling', '/cycling.jpg', 'A low-impact workout that improves cardiovascular health and endurance.'),
+    ('Gaming', '/gaming.jpg', 'A fun way to improve hand-eye coordination and reflexes.'),
+    ('Sailing', '/sailing.jpg', 'A relaxing way to enjoy the outdoors and improve balance.');
+`);
 }
+
+export default db;
